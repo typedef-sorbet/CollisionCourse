37177738048146d658d3cb1891b504ba89ff154e
warnespe001 <warnespe001@gmail.com>
2017-06-13 02:56:09 -0700

    Presentation Version

--- CollisionCourse.c
+++ CollisionCourse.c
@@ -6,24 +6,35 @@
 #include "assets.h"
 
 
-void drawInv();
-void drawMap(char *, char *, char *, char *, char *, char *);
-int checkForItem(char *);
-void removeItem(char*);
-char getCardinalDirection(char *str);
+
+
+void messHall();
 void bedroom();
 void controlRoom();
 void hatch();
+void engine();
+void controlPanel();
+void electrical();
+void cargo();
+void wirePanel();
+
+int checkForItem(char *);
+void removeItem(char*);
+void drawInv();
+void drawMap(char *, char *, char *, char *, char *, char *);
+char getCardinalDirection(char *str);
 void addItem(char*, char*);
 void examineItem(char*);
 char* toLowerCase(char*);
-void messHall();
 
 int max_x, max_y;
 int display_y, display_x, text_y, text_x, map_y, map_x, inv_y, inv_x;
 
 //a BUNCH of gamestate flags
-bool bedroomFirst = TRUE, controlFirst = TRUE, hatchFirst = TRUE, messHallFirst = TRUE;
+bool cargoFirst = TRUE, engineFirst = TRUE, electricalFirst = TRUE, bedroomFirst = TRUE, controlFirst = TRUE, hatchFirst = TRUE, messHallFirst = TRUE, controlPanelFirst = TRUE;
+bool wirePanelFixed = FALSE, engineWorking = FALSE;
+bool engineHatchLocked = TRUE;
+int batteryCharge = 0;
 
 //buffer for commands and text
 char command[30];
@@ -42,6 +53,9 @@ int main()
 	echo();
 	curs_set(FALSE);
 	cbreak();
+	start_color();
+	
+	init_pair(1, COLOR_GREEN, COLOR_BLACK);
 	
 	int enterSize = 3;
 	int otherWidth = 25;
@@ -118,13 +132,20 @@ void bedroom()
 	}
 	while(1)
 	{
-		wprintw(display, "\nA nightstand sits next to the bed pod, only inches away from the pillow.\nTo the south is the control room. A ladder leads upwards to a debug message.\n");
+		wprintw(display, "\nA nightstand sits next to the bed pod, only inches away from the pillow.\n");
+		if(checkForItem("Slippers") == -1)
+			wprintw(display, "Your comfy slippers sit at the edge of the bed, on a small rug.");
+		wprintw(display, "\nTo the south is the control room. A ladder leads upwards to a debug message.\n");
 		wrefresh(display);
 		wmove(textWindow, 1, 1);
 		wgetnstr(textWindow, command, 30);
 		wclear(textWindow);
 		wborder(textWindow, '|', '|', '-', '-', '+', '+', '+', '+');
 		wrefresh(textWindow);
+		
+		attron(COLOR_PAIR(1));
+		wprintw(display, "\n\t%s\n", command);
+		attroff(COLOR_PAIR(1));
 		
 		char token = getCardinalDirection(command);
 		switch(token)
@@ -162,12 +183,30 @@ void bedroom()
 				}
 				if(strstr(command, "look") != NULL && strstr(command, "nightstand") != NULL)
 				{
-					wprintw(display, "\nAn empty plastic cup sits atop the metal nightstand.\n");
+					if(checkForItem("Cup") == -1)
+						wprintw(display, "\nAn empty plastic cup sits atop the metal nightstand.\n");
+					else
+						wprintw(display, "\nThe metal nightstand sits alone and empty next to the bed.\n"
 				}
 				if(strstr(command, "cup") != NULL && checkForItem("Cup") == -1)
 				{
 					wprintw(display, "\nGotten.\n");
 					addItem("Cup", "A small plastic cup. Good for holding things.");
+				}
+				if(strstr(command, "slippers") != NULL && checkForItem("Slippers") == -1)
+				{
+					wprintw(display, "\nYou don the pink and fuzzy slippers, basking in true comfort.\n");
+					addItem("Slippers", "The pinnacle of luxury footwear.");
+				}
+				if(strstr(command, "rug") != NULL && checkForItem("Slippers") != -1)
+				{
+					if(checkForItem("Static") == -1)
+					{
+						wprintw(display, "\nAs you walk on the rug, you hear a faint clicking noise as static\nbegins to build up on your body.\n");
+						addItem("Static", "Who knew shuffling around on a rug could become so electric?");
+					}
+					else
+						wprintw(display, "\nYou've already built up enough static. Your hair's standing on end right now.\n");
 				}
 				break;
 		}
@@ -186,6 +225,14 @@ void controlRoom()
 	}
 	while(1)
 	{
+		if(!wirePanelFixed)
+		{
+			wprintw(display, "\nMost of the control panels in this room seem to be working, except for the\nmain control panel that activates the nav system. How inconvenient.\n");
+		}
+		else
+		{
+			wprintw(display, "\nThe main control panel happily blinks away, showing the current status of the ship.\n");
+		}
 		wprintw(display, "\nDoors lead out to the mess hall to the east, and the bedrooms to the north.\nA maintenance hatch is on the floor of the room, near the captain's chair.\n");
 		wrefresh(display);
 		wmove(textWindow, 1, 1);
@@ -193,6 +240,10 @@ void controlRoom()
 		wclear(textWindow);
 		wborder(textWindow, '|', '|', '-', '-', '+', '+', '+', '+');
 		wrefresh(textWindow);
+		
+		attron(COLOR_PAIR(1));
+		wprintw(display, "\n\t%s\n", command);
+		attroff(COLOR_PAIR(1));
 		
 		char token = getCardinalDirection(command);
 		switch(token)
@@ -212,6 +263,13 @@ void controlRoom()
 			default:
 				if(strstr(command, "hatch") != NULL)
 					hatch();		
+				if(strstr(command, "panel") != NULL && wirePanelFixed)
+				{
+					if(checkForItem("Access Code") != -1)
+						controlPanel();
+					else
+						wprintw(display, "\nThe control panel requires an access code.\n");
+				}
 				break;
 		}
 		
@@ -219,13 +277,119 @@ void controlRoom()
 	}
 }
 
+void controlPanel()
+{
+	wprintw(display, "\n");
+	drawMap(NULL, NULL, NULL, NULL, NULL, NULL);
+	if(controlPanelFirst)
+	{
+		wprintw(display, "\nYou type \"hunter2\" on the on screen keyboard, and a large \"ACCESS GRANTED\" message appears.\nYou're in.\n");
+		controlPanelFirst = FALSE;
+	}
+	while(1)
+	{
+		wprintw(display, "\nMAIN SHIP CONTROLS\nPlease select an action to take.\nA) Activate Main Thrusters\nB) Activate Self Destruct Sequence (for emergencies only!)\nC) Exit");
+		wrefresh(display);
+		wmove(textWindow, 1, 1);
+		wgetnstr(textWindow, command, 30);
+		wclear(textWindow);
+		wborder(textWindow, '|', '|', '-', '-', '+', '+', '+', '+');
+		wrefresh(textWindow);
+		
+		attron(COLOR_PAIR(1));
+		wprintw(display, "\n\t%s\n", command);
+		attroff(COLOR_PAIR(1));
+		
+		if(command[0] == 'A' || command[0] == 'a')
+		{
+			if(batteryCharge == 3 && engineWorking)
+			{
+				wprintw(display, "\nYou activate the main thrusters of the ship, and in the nick of time, the ship\ncorrects its course away from the asteroid into safety.\nYou can go back to sleep now. You've earned it. You're safe now.\n\nGOOD END\n");
+				wrefresh(display);
+				sleep(3);
+				endwin();
+				exit(0);
+			}
+			else
+			{
+				if(batteryCharge < 3)
+					wprintw(display, "\nError: Engine power is at suboptimal levels; thrusters cannot be activated.\n");
+				if(!engineWorking)
+					wprintw(display, "\nError: Engine currently nonfunctional. Notify maintenance crew immediately.\nWould you like to send an error report?\n");
+			}
+		}
+		else if(command[0] == 'B' || command[0] == 'b')
+		{
+			wprintw(display, "\nJust know that you did this to yourself.\n\nBAD END\n");
+			wrefresh(display);
+			sleep(3);
+			endwin();
+			exit(0);
+		}
+		else if(command[0] == 'C' || command[0] == 'c')
+		{
+			controlRoom();
+		}
+
+		wrefresh(display);
+	}
+}
+
+void wirePanel()
+{
+	wprintw(display, "\n");
+	drawMap(NULL, NULL, NULL, NULL, NULL, NULL);
+	//if(/*firstFlag*/)
+	//{
+	//	wprintw(display, "\nWIRE PANEL SKIP\n");
+	//	/*firstFlag*/ = FALSE;
+	//}
+	while(1)
+	{
+		wprintw(display, "\nWIRE PANEL REGULAR\n");
+		wrefresh(display);
+		wmove(textWindow, 1, 1);
+		wgetnstr(textWindow, command, 30);
+		wclear(textWindow);
+		wborder(textWindow, '|', '|', '-', '-', '+', '+', '+', '+');
+		wrefresh(textWindow);
+		
+		attron(COLOR_PAIR(1));
+		wprintw(display, "\n\t%s\n", command);
+		attroff(COLOR_PAIR(1));
+		
+		char token = getCardinalDirection(command);
+		switch(token)
+		{
+			case 'n':
+				break;
+			case 's':
+				break;
+			case 'e':
+				break;
+			case 'w':
+				break;
+			case 'u':
+				break;
+			case 'd':
+				break;
+			case 'q':
+				endwin();
+				exit(0);
+			default:		//additional checks
+				break;
+		}
+		wrefresh(display);
+	}
+}
+
 void hatch()
 {
 	wprintw(display, "\n");
-	drawMap("Hatch Part II", NULL, NULL, NULL, "Control Room", NULL);
+	drawMap("Cargo Bay", NULL, NULL, NULL, "Control Room", NULL);
 	if(hatchFirst)
 	{
-		wprintw(display, "The dimly lit corridor of the maintenance hatch is lined with steel pipes,\ndesigned to carry the blood of the ship.\nIn less literary terms, you're next to the fuel lines.\n");
+		wprintw(display, "The dimly lit corridor of the maintenance hatch is lined with steel pipes,\ndesigned to carry the blood of the ship.\nIn less literary terms, you're next to the fuel lines.\nInterestingly enough, someone seems to have scrawled some writing on the pipes.\n");
 		hatchFirst = FALSE;
 	}
 	while(1)
@@ -238,11 +402,15 @@ void hatch()
 		wborder(textWindow, '|', '|', '-', '-', '+', '+', '+', '+');
 		wrefresh(textWindow);
 		
+		attron(COLOR_PAIR(1));
+		wprintw(display, "\n\t%s\n", command);
+		attroff(COLOR_PAIR(1));
+		
 		char token = getCardinalDirection(command);
 		switch(token)
 		{
 			case 'n':
-				wprintw(display, "\nThats more of the hatch.\n");
+				cargo();
 				break;
 			case 'u':
 				controlRoom();	
@@ -251,6 +419,11 @@ void hatch()
 				endwin();
 				exit(0);
 			default:		
+				if(strstr(command, "look") != NULL && (strstr(command, "writing") != NULL || strstr(command, "pipe") != NULL) && checkForItem("Access Code") == -1)
+				{
+					wprintw(display, "\nAs you examine the writing on the pipes, you can faintly make out a string\nof characters. \"hunter2\". Perhaps this is a code?\nYou take note of this writng.\n");
+					addItem("Access Code" , "A code that might come in handy later. \"hunter2\".");
+				}
 				break;
 		}
 		
@@ -258,18 +431,18 @@ void hatch()
 	}
 }
 
-void messHall()
+void cargo()
 {
 	wprintw(display, "\n");
-	drawMap("Engineering Room", NULL, NULL, "Control Room", NULL, NULL);
-	if(messHallFirst)
+	drawMap(NULL, "Fuel Lines", "Engine Room", NULL, NULL, NULL);
+	if(cargoFirst)
 	{
-		wprintw(display, "\nThe mess hall is a complete mess. How apt.\n");
-		messHallFirst = FALSE;
+		wprintw(display, "\nBoxes, boxes, and more boxes. Toolboxes, wooden boxes, all kinds of boxes.\n");
+		cargoFirst = FALSE;
 	}
 	while(1)
 	{
-		wprintw(display, "\nIn the corner sits a machine labeled \"Food\". To the north is the engineering room, and to the\nwest is the control room.\n");
+		wprintw(display, "A toolbox lies open atop a waist-high crate.\nTo the south are the fuel lines and the maintenance hatch, and to\nthe east is the engine room.\n");
 		wrefresh(display);
 		wmove(textWindow, 1, 1);
 		wgetnstr(textWindow, command, 30);
@@ -277,11 +450,153 @@ void messHall()
 		wborder(textWindow, '|', '|', '-', '-', '+', '+', '+', '+');
 		wrefresh(textWindow);
 		
+		attron(COLOR_PAIR(1));
+		wprintw(display, "\n\t%s\n", command);
+		attroff(COLOR_PAIR(1));
+		
 		char token = getCardinalDirection(command);
 		switch(token)
 		{
 			case 'n':
-				//engineering();
+				break;
+			case 's':
+				hatch();
+				break;
+			case 'e':
+				engine();
+				break;
+			case 'w':
+				break;
+			case 'u':
+				break;
+			case 'd':
+				break;
+			case 'q':
+				endwin();
+				exit(0);
+			default:		//additional checks
+				if(strstr(command, "look") != NULL && strstr(command, "toolbox") != NULL)
+					wprintw(display, "\nInside the toolbox, you find a wrench amongst an array of broken tools.\n");
+				if(strstr(command, "wrench") != NULL && checkForItem("Wrench") == -1)
+				{
+					wprintw(display, "\nPicked up the wrench.\n");
+					addItem("Wrench", "It's like a wrench, but more...space-y.");
+				}
+				break;
+		}
+		wrefresh(display);
+	}
+}
+
+void engine()
+{
+	wprintw(display, "\n");
+	drawMap(NULL, NULL, NULL, "Cargo Bay", "Electrical Room", NULL);
+	if(engineFirst)
+	{
+		wprintw(display, "\nAs you enter this room, you hear the familiar hum of the engine hard\nat work...Wait, no you don't. That might be a problem.\n");
+		engineFirst = FALSE;
+	}
+	while(1)
+	{
+		if(engineWorking)
+		{
+			wprintw(display, "\nThe engine on the far wall spins its gears happily, supplying power to the thrusters.\n");
+		}
+		else
+		{
+			wprintw(display, "\nThe engine on the far wall groans painfully, its gears seemingly stuck.\n");
+		}
+		wprintw(display, "/nTo the west is the cargo bay. Above you, there is a hatch to the electrical room./n");
+		wrefresh(display);
+		wmove(textWindow, 1, 1);
+		wgetnstr(textWindow, command, 30);
+		wclear(textWindow);
+		wborder(textWindow, '|', '|', '-', '-', '+', '+', '+', '+');
+		wrefresh(textWindow);
+		
+		attron(COLOR_PAIR(1));
+		wprintw(display, "\n\t%s\n", command);
+		attroff(COLOR_PAIR(1));
+		
+		char token = getCardinalDirection(command);
+		switch(token)
+		{
+			case 'n':
+				break;
+			case 's':
+				break;
+			case 'e':
+				break;
+			case 'w':
+				cargo();
+				break;
+			case 'u':
+				if(engineHatchLocked)
+					wprintw(display, "\nThe hatch appears to be locked from the other side.\n");
+				else
+					electrical();
+				break;
+			case 'd':
+				break;
+			case 'q':
+				endwin();
+				exit(0);
+			default:		//additional checks
+				if(strstr(command, "look") != NULL && strstr(command, "engine") != NULL)
+				{
+					if(engineWorking)
+					{
+						wprintw(display, "\nThe engine is working happily to make the ship work.\nLet's keep it that way.\n");
+					}
+					else
+						wprintw(display, "\nThe gears of the engine are stuck, and appear to not be able to move past\none another. Perhaps some lubricant will fix the problem.\n");
+				}
+				if((strstr(command, "engine") != NULL || strstr(command, "gear") != NULL) && (strstr(command, "slim") != NULL || strstr(command, "lub") != NULL || strstr(command, "food") != NULL))
+				{
+					if(!engineWorking && checkForItem("Cup w/ Slime") != -1)
+					{
+						wprintw(display, "\nYou pour the slime onto the gears, allowing it to seep into every\nnook and cranny. The gears begin to croak to life, spinning\nhappily and getting the ship back on its metaphorical feet.\n");
+						removeItem("Cup w/ Slime");
+						addItem("Cup", "A small plastic cup. Good for holding things.");
+					}
+					else
+						wprintw(display, "\nYou don't need to mess with the engine anymore.\n");
+				}
+				break;
+		}
+		wrefresh(display);
+	}
+}
+
+void messHall()
+{
+	wprintw(display, "\n");
+	drawMap("Electrical Room", NULL, NULL, "Control Room", NULL, NULL);
+	if(messHallFirst)
+	{
+		wprintw(display, "\nThe mess hall is a complete mess. How apt.\n");
+		messHallFirst = FALSE;
+	}
+	while(1)
+	{
+		wprintw(display, "\nIn the corner sits a machine labeled \"Food\". To the north is the electrical room, and to the\nwest is the control room.\n");
+		wrefresh(display);
+		wmove(textWindow, 1, 1);
+		wgetnstr(textWindow, command, 30);
+		wclear(textWindow);
+		wborder(textWindow, '|', '|', '-', '-', '+', '+', '+', '+');
+		wrefresh(textWindow);
+		
+		attron(COLOR_PAIR(1));
+		wprintw(display, "\n\t%s\n", command);
+		attroff(COLOR_PAIR(1));
+		
+		char token = getCardinalDirection(command);
+		switch(token)
+		{
+			case 'n':
+				electrical();
 				break;
 			case 's':
 				break;
@@ -321,6 +636,113 @@ void messHall()
 	}
 }
 
+void electrical()
+{
+	wprintw(display, "\n");
+	drawMap(NULL, "Mess Hall", NULL, NULL, NULL, "Engine Room");
+	if(electricalFirst)
+	{
+		wprintw(display, "\nI would describe this room to you in detail, but I'm a computer science major.\nIf you want a detailed description of the electrical workings of a spaceship, you\nshould play a game made by an electrical engineer, not me.\n");
+		electricalFirst = FALSE;
+	}
+	while(1)
+	{
+		wprintw(display, "\nA large battery sits in the middle of the room, with an exposed metal panel and a\nsign labeled \"Engine Power\". On the wall behind the battery, a panel is loosely\nbolted to the wall, covering some wires.\nA locked hatch leads downwards to the engine room, and the mess hall is to the south.\n");
+		wrefresh(display);
+		wmove(textWindow, 1, 1);
+		wgetnstr(textWindow, command, 30);
+		wclear(textWindow);
+		wborder(textWindow, '|', '|', '-', '-', '+', '+', '+', '+');
+		wrefresh(textWindow);
+		
+		attron(COLOR_PAIR(1));
+		wprintw(display, "\n\t%s\n", command);
+		attroff(COLOR_PAIR(1));
+		
+		char token = getCardinalDirection(command);
+		switch(token)
+		{
+			case 'n':
+				break;
+			case 's':
+				messHall();
+				break;
+			case 'e':
+				break;
+			case 'w':
+				break;
+			case 'u':
+				break;
+			case 'd':
+				if(checkForItem("Silver Key") != -1)
+				{
+					if(engineHatchLocked)
+					{
+						wprintw(display, "\nYou use the silver key on the locked hatch, and it fits!");
+						engineHatchLocked = FALSE;
+					}
+					//engine();
+				}
+				break;
+			case 'q':
+				endwin();
+				exit(0);
+			default:		//additional checks
+				if(strstr(command, "hatch") != NULL)
+				{
+					if(checkForItem("Silver Key") != -1)
+					{
+						if(engineHatchLocked)
+						{
+							wprintw(display, "\nYou use the silver key on the locked hatch, and it fits!");
+							engineHatchLocked = FALSE;
+						}
+						engine();
+					}
+				}
+				if(strstr(command, "panel") != NULL)
+				{
+					if(checkForItem("Wrench") != -1)
+					{
+						if(!wirePanelFixed)
+							wirePanel();
+						else
+							wprintw(display, "\nThe panel is fixed, you don't need to mess with it anymore.\n");
+					}
+					else
+					{
+						wprintw(display, "\nYou don't seem to have a way of removing the panel cover.\nAnd before you ask: no, you're not strong enough to rip it off.\n");
+					}
+				}
+				if(strstr(command, "look") != NULL && strstr(command, "battery") != NULL)
+				{
+					wprintw(display, "\nThe battery has an exposed metal panel, which appears to be connected to the\narea where the charge is stored.\n");
+				}
+				if(strstr(command, "touch") != NULL && (strstr(command, "battery") != NULL))
+				{
+					if(batteryCharge < 3)
+					{
+						if(checkForItem("Static") != -1)
+						{
+							++batteryCharge;
+							removeItem("Static");
+							if(batteryCharge == 1)
+								wprintw(display, "\nAn indicator on the battery shows that it is one third charged.\n");
+							else if(batteryCharge == 2)
+								wprintw(display, "\nThe indicator now shows that the battery is two thirds charged.\n");
+							else
+								wprintw(display, "\nThe battery is now fully charged.\n");
+						}
+					}
+					else
+						wprintw(display, "\nThe battery is already charged.\n");
+				}
+				break;
+		}
+		wrefresh(display);
+	}
+}
+
 void drawInv()
 {
 	mvwprintw(invWin, 1, 1, "Inventory");
@@ -344,6 +766,7 @@ void removeItem(char *itemName)
 			inv.items[indexOfItem] = inv.items[indexOfItem+1];
 	}
 	inv.numItems--;
+	drawInv();
 }
 
 //should the indexes be hardcoded? no.
